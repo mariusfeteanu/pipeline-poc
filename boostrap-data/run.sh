f25fa3bd043c456a554521c9cd3209fb66814c89
marius <marius.feteanu@gmail.com>
2025-09-28 19:46:52 +0100

    papers bootstrap

--- boostrap-data/run.sh
+++ boostrap-data/run.sh
@@ -1,39 +1,7 @@
 #!/usr/bin/env bash
 
-MANIFEST_URL="https://www.eia.gov/opendata/bulk/manifest.txt"
-MANIFEST_FILE="manifest.json"
-[ -f "$MANIFEST_FILE" ] || curl -s -L -A "Mozilla/5.0" "$MANIFEST_URL" | jq . > "$MANIFEST_FILE"
-
-DATASETS_FILE="datasets"
-mkdir -p ../.data
-
-while read -r dataset; do
-    [ -z "$dataset" ] && continue
-    url=$(jq -r ".dataset[\"$dataset\"].accessURL" "$MANIFEST_FILE")
-    if [ -z "$url" ] || [ "$url" = "null" ]; then
-        echo "No URL found for $dataset"
-        continue
-    fi
-
-    zipfile="../.data/$(basename "$url")"
-    target_dir="../.data/$dataset"
-
-    if [ -d "$target_dir" ] || [ -f "$zipfile" ]; then
-        echo "Skipping downloading $dataset (already extracted in $target_dir)"
-    else
-        echo "Downloading $dataset..."
-        curl -L -A "Mozilla/5.0" -o "$zipfile" "$url"
-    fi
-
-    if [ -d "$target_dir" ]; then
-        echo "Skipping extracting $dataset (already extracted in $target_dir)"
-    else
-        echo "Extracting $dataset..."
-        mkdir -p "$target_dir"
-        unzip -q "$zipfile" -d "$target_dir"
-    fi
-
-done < "$DATASETS_FILE"
+. gov.sh
+. papers.sh "cs.CL"
+. papers.sh "physics.pop-ph"
 
 du -h ../.data
-
