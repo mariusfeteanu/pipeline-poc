36363c5d39154d0917b276264b12afd0ed8c59c7
marius <marius.feteanu@gmail.com>
2025-09-29 16:57:19 +0100

    fixed wait

--- api_document/api_document/main.py
+++ api_document/api_document/main.py
@@ -3,28 +3,28 @@ import logging
 import time
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from kafka import KafkaProducer  # type: ignore
 from pydantic import BaseModel
-
-producer: KafkaProducer | None = None
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    global producer
-    for i in range(5):
+    for i in range(10):
         try:
             producer = KafkaProducer(
                 bootstrap_servers="kafka:9092",
                 value_serializer=lambda v: json.dumps(v).encode("utf-8"),
             )
+            app.state.producer = producer
             producer.bootstrap_connected()
             logging.info("Connected to Kafka")
             break
         except Exception as e:
             logging.warning(f"Kafka not available yet, retrying... ({i+1})")
-            time.sleep(5)
+            time.sleep(i)
+    else:
+        app.state.producer = None
 
     yield  # yield to application
 
@@ -43,8 +43,9 @@ class DocumentLandedEvent(BaseModel):
 
 @app.post("/document/v1")
 def create_item_event(doc: DocumentLandedEvent):
-    global producer
-    assert producer is not None, "Producer not initialized"
+    producer: KafkaProducer | None = app.state.producer
+    if producer is None:
+        raise HTTPException(status_code=503, detail="Kafka not ready")
     event = doc.model_dump(mode="json")
     logging.info(f"Sending event to Kafka: {event}")
     producer.send("document-landed", event)
